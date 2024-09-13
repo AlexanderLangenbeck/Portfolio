ac35ce50bf6637df5a26f3839189cc5325e1d050
Alexander Langenbeck <alex.langenbeck@gmx.de>
2024-09-13 06:40:41 +0200

    wrapperEdit

--- src/app/landing-page/welcome/welcome-header/welcome-header.component.ts
+++ src/app/landing-page/welcome/welcome-header/welcome-header.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './welcome-header.component.html',
   styleUrl: './welcome-header.component.scss'
 })
 export class WelcomeHeaderComponent {
+  navbarActive: boolean = false;
+  @ViewChild('navbar') navbar!: ElementRef;
+
+  openWrapper() {
+    this.navbarActive = !this.navbarActive;
+    if (this.navbarActive) {
+      this.navbar.nativeElement.style.display = 'flex';
+      this.navbar.nativeElement.classList.add('headerNavbar');
+    } else {
+      this.navbar.nativeElement.style.display = 'none';
+    }
+  }
+
+  closeWrapper(){
+    this.navbar.nativeElement.style.display = 'none';
+  }
 
 }
